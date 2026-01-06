04aaed7ee4034007e3b8d4e36b2d1696947a23b9
assislol2024 <assislol2024@gmail.com>
2026-01-06 17:47:56 -0300

    correção

--- js/dom.js
+++ js/dom.js
@@ -1,19 +1,69 @@
-import GerenciadorAlunos from 'gerenciadorAlunos.js'
-import localStorageVerificacao from 'validação.js'
+// ---obj---
+class GerenciadorAlunos {
+    
+    constructor(){
+        this.alunos = [],
+        this.media = 7
+    }
+
+    adicionarAluno(nome, nota){
+        nota = Number(nota);
+
+        if(nome === "" || isNaN(nota) || nota < 0 || nota > 10){
+            return "Dados inválidos";
+        }
 
 
+        this.alunos.push({ nome:nome, nota:nota });
+    }
+
+    mediaGeral(){
+        let soma = 0;
+        this.alunos.forEach((aluno) => {
+            soma += aluno.nota;
+        });
+
+        let media = soma / this.alunos.length;
+        return Number(media.toFixed(2));
+    }
+
+    aprovados() {
+        return this.alunos.filter(aluno => aluno.nota >= this.media);
+    }
+
+    reprovados() {
+        return this.alunos.filter(aluno => aluno.nota < this.media);
+    }
+
+
+    removerAluno(nome){
+        this.alunos = this.alunos.filter(aluno => aluno.nome !== nome);
+    }
+
+
+};
 let objAlunos = new GerenciadorAlunos();
-console.log(objAlunos)
 
 
+
+// ---verificação---
+function localStorageVerificacao() {
+    if (localStorage.getItem(0) === null) {
+        localStorage.setItem(0, JSON.stringify([]));
+        objAlunos.alunos = [];
+    } else {
+        objAlunos.alunos = JSON.parse(localStorage.getItem(0));
+    }
+}; 
+localStorageVerificacao()
+// --------------------DOM----------------
 const btnAdicionar = document.querySelector("#btnAdicionar");
 const btnAprovados = document.querySelector("#btnAprovados");
 const btnReprovados = document.querySelector("#btnReprovados");
 const mensagem = document.querySelector(".mensagem")
 const lista = document.querySelector(".lista");
 const media = document.querySelector("#mediaValue");
-// ---verificação---
-localStorageVerificacao()
+
 
 btnAdicionar.addEventListener("click", (e) => {
     e.preventDefault();
@@ -26,7 +76,8 @@ btnAdicionar.addEventListener("click", (e) => {
         mensagem.innerHTML = "O valor de nota é entre 0 e 10";
     }else{
         objAlunos.adicionarAluno(nome, nota);
-        localStorage.setItem(0, JSON.stringify(objAlunos.alunos))
+
+        localStorage.setItem(0, JSON.stringify(objAlunos.alunos));
 
         document.querySelector("#nome").value = "";
         document.querySelector("#nota").value = "";
@@ -39,12 +90,19 @@ btnAdicionar.addEventListener("click", (e) => {
 
 btnAprovados.addEventListener("click", (e) => {
     e.preventDefault();
-    mensagem.innerHTML = "";
-    lista.innerHTML = `${objAlunos.aprovados()}`;
+    const aprovados = objAlunos.aprovados();
+
+    lista.innerHTML = aprovados.length
+        ? aprovados.map(a => `${a.nome} - ${a.nota}`).join("<br>")
+        : "Nenhum aluno aprovado";
 });
 
 btnReprovados.addEventListener("click", (e) => {
     e.preventDefault();
-    mensagem.innerHTML = "";
-    lista.innerHTML = objAlunos.reprovados();
+    const reprovados = objAlunos.reprovados();
+
+    lista.innerHTML = reprovados.length
+        ? reprovados.map(a => `${a.nome} - ${a.nota}`).join("<br>")
+        : "Nenhum aluno reprovado";
+
 });
